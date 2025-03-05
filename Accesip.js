57faf3b00cd14fc6a681e6019b1ea5e088935b04
ndikzone <baksocloud@gmail.com>
2025-03-05 21:03:21 +0700

    Create Accesip.js

---
+++ Accesip.js
@@ -0,0 +1,78 @@
+{
+  "ip": "From: ::ffff:127.0.0.1",
+  "port": "Port: 3636",
+  "method": "Method: GET",
+  "url": "URL: /AccessIP",
+  "headers": {
+    "host": "127.0.0.1:3636",
+    "connection": "close",
+    "cf-ray": "91ba21a8d873fd31-SIN",
+    "x-forwarded-for": "103.87.148.96",
+    "user-agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Mobile Safari/537.36",
+    "priority": "u=0, i",
+    "accept-encoding": "gzip, br",
+    "cf-ipcountry": "ID",
+    "x-forwarded-proto": "https",
+    "if-none-match": "W/\"873-y5Hfv1Ftv/mwAs3Iv6cc+7AOxrM\"",
+    "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+    "cf-visitor": "{\"scheme\":\"https\"}",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "cf-connecting-ip": "103.87.148.96",
+    "sec-fetch-site": "none",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "cdn-loop": "cloudflare; loops=1",
+    "sec-ch-ua": "\"Not(A:Brand\";v=\"99\", \"Google Chrome\";v=\"133\", \"Chromium\";v=\"133\"",
+    "sec-ch-ua-mobile": "?1",
+    "sec-ch-ua-platform": "\"Android\"",
+    "upgrade-insecure-requests": "1",
+    "cookie": "cf_clearance=Cp9blqCeTgwra_IkW5_i1XzAXrg0zfsu3dhpb4KLu_A-1741178189-1.2.1.1-4G1uhlY_6cs48htE5iOtHcetlDNO3cBwE27ThhqAdfI0x2zNZdD2b7wBP9lDBveBURTPxC645Zt4OCLbJ8A52bM2JiHa1cHoB53qO6_RGzqlsal5E8iRdcH.vkev_Gisho_QIidbvoz42UHIq6TCe893RxkRAGZNAujlJNB1uox6oTEMdWEScuCuhuJORrhSn.uhzURe_gPK7YeO_wXUg5DBizufSrp9ujS2GURFtBotYYjeNDLvzkaeXexeVrgPBgkVF6P0ChxQSoe2LyaEEJeesbwPUfMvMtDTcS78QItr_5TExWRig_4jhbkWKhAN_HOa6vXm_HRvf_NgcisLZ9GIRdIHNPAUQ7T292TdmtxK_VdKZsQEUntCn.36_3V9U_63uoICUHJEEABlHs5vi1a7gFpXRI5tdWRBAdk45pw"
+  },
+  "body": {
+
+  },
+  "data": {
+    "IP_access_Script": [
+      {
+        "ip": "192.168.0.1",
+        "status": "tidakdiverif"
+      },
+      {
+        "ip": "209.97.168.38",
+        "status": "terverifikasi"
+      },
+      {
+        "ip": "206.183.131.172",
+        "status": "terverifikasi"
+      },
+      {
+        "ip": "202.157.185.130",
+        "status": "terverifikasi"
+      },
+      {
+        "ip": "167.172.255.117",
+        "status": "terverifikasi"
+      },
+      {
+        "ip": "128.199.241.157",
+        "status": "terverifikasi"
+      },
+      {
+        "ip": "159.65.140.153",
+        "status": "terverifikasi"
+      },
+      {
+        "ip": "159.223.81.33",
+        "status": "terverifikasi"
+      },
+      {
+        "ip": "159.223.81.221",
+        "status": "terverifikasi"
+      },
+      {
+        "ip": "165.22.50.33",
+        "status": "terverifikasi"
+      }
+    ]
+  }
+}
